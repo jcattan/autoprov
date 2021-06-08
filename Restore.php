f917388cc49f8fb831d7bcc093f932f8a8ac38ba
J.Cattan <freepbx@famillecattan.com>
2021-06-08 16:16:16 +0200

    ajout backup restore fpbx 15

---
+++ Restore.php
@@ -0,0 +1,25 @@
+<?php
+namespace FreePBX\modules\autoprov;
+use FreePBX\modules\Backup as Base;
+class Restore Extends Base\RestoreBase{
+    public function runRestore()
+    {
+        $configs = $this->getConfigs();
+        if( ! empty($configs['tables']) && is_array($configs['tables']) )
+        {
+            $this->importTables($configs['tables']);
+        }
+		 $files = $this->getFiles();
+		 foreach ($files as $file) {
+                        $filename = $file->getPathTo().'/'.$file->getFilename();
+                        $source = $this->tmpdir.'/files'.$file->getPathTo().'/'.$file->getFilename();
+                        $dest = $filename;
+                        if(file_exists($source)){
+                                @mkdir($file->getPathTo(),0755,true);
+                                copy($source, $dest);
+                        }
+		 }
+
+    }
+
+}
