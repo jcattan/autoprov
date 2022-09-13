33862b4d6ed14a015a65785d2270f064b8daf940
J.Cattan <freepbx@famillecattan.com>
2022-09-13 10:40:32 +0200

    ajout t3x

--- _ap_phone_modules/endpoint/yealinkv80/t3x/phone.php
+++ _ap_phone_modules/endpoint/yealinkv80/t3x/phone.php
@@ -53,8 +53,8 @@ class endpoint_yealinkv80_t3x_phone extends endpoint_yealinkv80_base {
 
     function prepare_for_generateconfig() {
 		$this->settings['call_pickup'] = isset($this->settings['call_pickup']) ? $this->settings['call_pickup'] : '*8';
-        # This contains the last 2 digits of y0000000000xx.cfg, for each model.
-        $model_suffixes = array('T38' => '38', 'T32' => '32');
+        # This contains the last 3 digits of y000000000xxx.cfg, for each model.
+        $model_suffixes = array('T38' => '038', 'T32' => '032', 'T33' => '124');
         //Yealink likes lower case letters in its mac address
         $this->mac = strtolower($this->mac);
         $this->config_file_replacements['$suffix'] = $model_suffixes[$this->model];
