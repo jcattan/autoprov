21fe3ad0e8b42eadaa26b1e76c37129387c1a700
J.Cattan <freepbx@famillecattan.com>
2022-09-13 10:38:47 +0200

    bug
    correction bug backup db

--- Backup.php
+++ Backup.php
@@ -5,35 +5,18 @@ use RecursiveIteratorIterator;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
-	  		$configs = $this->dumpAll();
-		$this->addConfigs($configs);
-        // $this->addDirectories([
-		// '/var/www/html/admin/modules/_ap_phone_modules',
-		// ]);
-			// $files = glob("/var/www/html/admin/modules/_ap_phone_modules/*");
-        // foreach ($files as $file) {
-            // $path = pathinfo($file, PATHINFO_DIRNAME);
-            // $this->addFile(basename($file), $path, '', "conf");
-        // }
-        // return $this;
-
-		//$this->FreePBX;
-		//$this->addDirectories($dirs);
-		//$this->dumpTables();
-		
-		 $dirs = [];
+                $tables = $this->dumpTables();
+                $configs = [
+                        'tables' => $tables
+                ];
+        $dirs = [];
                 $varwww = $this->FreePBX->Config->get('AMPWEBROOT');
                 $iterator = new RecursiveDirectoryIterator($varwww.'/admin/modules/_ap_phone_modules',RecursiveDirectoryIterator::SKIP_DOTS);
                 foreach (new RecursiveIteratorIterator($iterator) as $file) {
                                 $dirs[] = $file->getPath();
                                 $this->addFile($file->getBasename(),$file->getPath(),'',"_ap_phone_modules");
-
                 }
                 $this->addDirectories(array_unique($dirs));
-                $this->addConfigs([
-                        'data' => $this->FreePBX->Music->getCategories(),
-                        'settings' => $this->dumpAdvancedSettings()
-                ]);
+				$this->addConfigs($configs);
         }
-
 }
